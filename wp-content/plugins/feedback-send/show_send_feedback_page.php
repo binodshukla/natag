deebb2a8ba643f5a6ab135033aaa99a7f8cf43d6
Binod Shukla <binod@thewebdevelopers.co.in>
2013-02-06 12:57:46 +0530

    Feedback Plugin update

--- wp-content/plugins/feedback-send/show_send_feedback_page.php
+++ wp-content/plugins/feedback-send/show_send_feedback_page.php
@@ -1,6 +1,35 @@
 <?php
 	global $wpdb;
 
+	if($_POST['action'] == "send")
+	{
+		if(count($_POST['users']) > 0)
+		{
+			// Empty Last Records
+			$wpdb->query('TRUNCATE TABLE wp_send_feedback');
+			
+			foreach($_POST['users'] as $user_id)
+			{
+				// Start Mailing
+				$user_info = get_userdata($user_id);
+				$to = $user_info->user_email;
+				$name = ucfirst($user_info->display_name);
+				$subject = "Feedback Survey Natag";
+				$message = eval($_POST['content']);
+				
+				$headers = 'From: National AG';
+				$headers  .= 'MIME-Version: 1.0' . "\r\n";
+				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+				$mail = mail( $to, $subject, $message, $headers);
+				if($mail)
+				{
+					$insert_query = "insert into wp_send_feedback (user_id, feedback_status) values ($user_id)";
+					$wpdb->query($insert_query);
+				}
+			}
+		}
+	}
+	
 	add_action('admin_print_scripts', 'do_jslibs' );
 	add_action('admin_print_styles', 'do_css' );
 	
@@ -15,6 +44,7 @@
 		wp_enqueue_script('thickbox');
 		add_action( 'admin_head', 'wp_tiny_mce' );
 	}
+	
 ?>
 <div class="icon32">
     <img width="32" height="32" alt="icon" src="http://localhost/natag/wp-content/plugins/wp-feedback-survey-manager/static/admin/images/feedback_32.png">
@@ -32,7 +62,7 @@
 					$blogusers = get_users('orderby=nicename&role=formar');
 					foreach ($blogusers as $user) {
 						?>
-                        <option value="<?php echo $user->ID; ?>"><?php echo $user->user_email; ?></option>
+                        <option value="<?php echo $user->ID; ?>"><?php echo $user->display_name; ?> < <?php echo $user->user_email; ?> ></option>
                         <?php
 					}
 					?>
@@ -48,6 +78,7 @@
         </tr>
         <tr>
         	<td colspan="2" align="right">
+            	<input type="hidden" name="action" value="send" />
             	<input type="submit" name="submit" value="Send" class="button-primary">
             </td>
         </tr>
